ea819f9ea73c871aafdc5cb0653bd930c831c8af
Holstad <ola.holstad@gmail.com>
2016-02-08 15:20:04 +0100

    Directions, multi waypionts

--- PrototypeNavigator/app/src/main/java/se/jolo/prototypenavigator/MainActivity.java
+++ PrototypeNavigator/app/src/main/java/se/jolo/prototypenavigator/MainActivity.java
@@ -1,25 +1,168 @@
 package se.jolo.prototypenavigator;
 
-import android.app.Activity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.mapbox.directions.DirectionsCriteria;
+import com.mapbox.directions.MapboxDirections;
+import com.mapbox.directions.service.models.DirectionsResponse;
+import com.mapbox.directions.service.models.DirectionsRoute;
+import com.mapbox.directions.service.models.Waypoint;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdate;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String LOG_TAG = "MainActivity";
+
+    private final static String MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoicHJvdG90eXBldGVhbSIsImEiOiJjaWs2bXQ3Y3owMDRqd2JtMTZsdjhvbzVnIn0.NBH7u7RG-lqxGq_PEIjFjw";
 
     private MapView mapView = null;
+    private DirectionsRoute currentRoute = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Dupont Circle (Washington, DC)
+        Waypoint origin = new Waypoint(-77.04341, 38.90962);
+
+        // The White House (Washington, DC)
+        Waypoint destination = new Waypoint(-77.0365, 38.8977);
+
+        // The White House (Washington, DC)
+        Waypoint destination2 = new Waypoint(-77.1365, 38.8977);
+
+        List<Waypoint> waypoints = new ArrayList<Waypoint>();
+
+        waypoints.add(destination2);
+        waypoints.add(origin);
+        waypoints.add(destination);
+
+
+        // Centroid
+        LatLng centroid = new LatLng(
+                (origin.getLatitude() + destination.getLatitude()) / 2,
+                (origin.getLongitude() + destination.getLongitude()) / 2);
+
+        // Set up a standard Mapbox map
         mapView = (MapView) findViewById(R.id.mapboxMapView);
+        mapView.setAccessToken(MAPBOX_ACCESS_TOKEN);
         mapView.setStyleUrl(Style.MAPBOX_STREETS);
-        mapView.setCenterCoordinate(new LatLng(40.73581, -73.99155));
-        mapView.setZoomLevel(11);
+        mapView.setCenterCoordinate(centroid);
+
+        mapView.setZoomLevel(10);
         mapView.onCreate(savedInstanceState);
+
+        // We're gonna use this to demo off-route detection
+        mapView.setOnMapClickListener(new MapView.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng point) {
+                Waypoint target = new Waypoint(point.getLongitude(), point.getLatitude());
+                checkOffRoute(target);
+            }
+        });
+
+        // Add origin and destination to the map
+        mapView.addMarker(new MarkerOptions()
+                .position(new LatLng(origin.getLatitude(), origin.getLongitude()))
+                .title("Origin")
+                .snippet("Dupont Circle"));
+
+        mapView.addMarker(new MarkerOptions()
+                .position(new LatLng(destination.getLatitude(), destination.getLongitude()))
+                .title("Destination")
+                .snippet("The White House"));
+
+        mapView.addMarker(new MarkerOptions()
+                .position(new LatLng(destination2.getLatitude(), destination2.getLongitude()))
+                .title("Destination2")
+                .snippet("The White House2"));
+
+        // Get route from API
+        getRoute(waypoints);
+
+
+        //mapView.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(destination.getLatitude(),destination.getLongitude()),11,45,11)));
+
+    }
+
+    private void getRoute(List<Waypoint>waypoints) {
+        MapboxDirections md = new MapboxDirections.Builder()
+                .setAccessToken(MAPBOX_ACCESS_TOKEN)
+                .setWaypoints(waypoints)
+                .setProfile(DirectionsCriteria.PROFILE_WALKING)
+                .build();
+
+        md.enqueue(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Response<DirectionsResponse> response, Retrofit retrofit) {
+                // You can get generic HTTP info about the response
+                Log.d(LOG_TAG, "Response code: " + response.code());
+
+                // Print some info about the route
+                currentRoute = response.body().getRoutes().get(0);
+                Log.d(LOG_TAG, "Distance: " + currentRoute.getDistance());
+                showMessage(String.format("Route is %d meters long.", currentRoute.getDistance()));
+
+                // Draw the route on the map
+                drawRoute(currentRoute);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e(LOG_TAG, "Error: " + t.getMessage());
+                showMessage("Error: " + t.getMessage());
+            }
+        });
+    }
+
+    private void drawRoute(DirectionsRoute route) {
+        // Convert List<Waypoint> into LatLng[]
+        List<Waypoint> waypoints = route.getGeometry().getWaypoints();
+        LatLng[] point = new LatLng[waypoints.size()];
+        for (int i = 0; i < waypoints.size(); i++) {
+            point[i] = new LatLng(
+                    waypoints.get(i).getLatitude(),
+                    waypoints.get(i).getLongitude());
+        }
+
+        // Draw Points on MapView
+        mapView.addPolyline(new PolylineOptions()
+                .add(point)
+                .color(Color.parseColor("#3887be"))
+                .width(5));
+    }
+
+    private void checkOffRoute(Waypoint target) {
+        if (currentRoute.isOffRoute(target)) {
+            showMessage("You are off-route.");
+        } else {
+            showMessage("You are not off-route.");
+        }
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override

7a4d18be6cef5a021b667f4e9ee148766a944979
SuperSpaceNinja <phiphigt@gmail.com>
2016-02-25 13:43:32 +0100

    routing moved out of Map Activity

--- PrototypeNavigator/app/src/main/java/se/jolo/prototypenavigator/Router.java
+++ PrototypeNavigator/app/src/main/java/se/jolo/prototypenavigator/Router.java
@@ -1,5 +1,6 @@
 package se.jolo.prototypenavigator;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.location.Location;
 import android.util.Log;
@@ -10,7 +11,6 @@ import com.mapbox.directions.MapboxDirections;
 import com.mapbox.directions.service.models.DirectionsResponse;
 import com.mapbox.directions.service.models.DirectionsRoute;
 import com.mapbox.directions.service.models.Waypoint;
-import com.mapbox.mapboxsdk.annotations.Polyline;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
@@ -22,6 +22,8 @@ import java.util.List;
 import retrofit.Callback;
 import retrofit.Response;
 import retrofit.Retrofit;
+import se.jolo.prototypenavigator.model.Route;
+import se.jolo.prototypenavigator.model.RouteItem;
 
 /**
  * Created by Joel on 2016-02-24.
@@ -29,19 +31,51 @@ import retrofit.Retrofit;
 public final class Router {
 
     private static final String LOG_TAG = "ROUTER";
+    private Context context;
     private MapView mapView;
     private String MAPBOX_ACCESS_TOKEN = "";
     private Location currentLocation;
-    private DirectionsRoute directionsRoute;
+    private DirectionsRoute currentRoute, fullRoute;
+    private List<Waypoint> waypoints;
     private List<Waypoint> waypointsRemaining;
     private boolean inProximity;
-    PolylineOptions polylineToNextStop;
+    private PolylineOptions polylineToNextStop;
 
-    public Router(MapView mapView, String MAPBOX_ACCESS_TOKEN) {
+    public Router(Context context, MapView mapView, String MAPBOX_ACCESS_TOKEN) {
+        this.context = context;
         this.mapView = mapView;
         this.MAPBOX_ACCESS_TOKEN = MAPBOX_ACCESS_TOKEN;
         this.inProximity = false;
     }
+
+    private List<Waypoint> fewerWaypoints() {
+
+        List<Waypoint> fewerWaypoints = new ArrayList<>();
+
+        fewerWaypoints.add(waypoints.get(0));
+        fewerWaypoints.add(waypoints.get(1));
+        fewerWaypoints.add(waypoints.get(2));
+        fewerWaypoints.add(waypoints.get(3));
+        fewerWaypoints.add(waypoints.get(4));
+
+        return fewerWaypoints;
+    }
+
+    public Router loadWaypoints(Route route) {
+
+        waypoints = new ArrayList<>();
+        List<RouteItem> routeItems = route.getRouteItems();
+
+        for (RouteItem ri : routeItems) {
+            waypoints.add(new Waypoint(
+                    ri.getStopPoint().getEasting(),
+                    ri.getStopPoint().getNorthing()));  // altitude can be set as a third parameter
+        }
+
+        setWaypointsRemaining(waypoints);
+
+        return this;
+    } 
 
     public Router updateWaypointsRemaining() {
 
@@ -53,95 +87,12 @@ public final class Router {
         return this;
     }
 
-    public Router setWaypoints(List<Waypoint> waypoints) {
-        waypointsRemaining = waypoints;
-        return this;
-    }
-
-    public List<Waypoint> getCurrentRoute() {
-
-        List<Waypoint> positionAndNextWaypoint = new ArrayList<>();
-
-        positionAndNextWaypoint.add(new Waypoint(currentLocation.getLongitude(), currentLocation.getLatitude()));
-        positionAndNextWaypoint.add(waypointsRemaining.get(0));
-
-        return positionAndNextWaypoint;
-    }
-
     public Router checkWaypointProximity() {
 
-        if (directionsRoute.getDistance() <= 0.05) {
+        if (currentRoute.getDistance() <= 0.05) {
             Log.d(LOG_TAG, "you're within 0.05 mile of the next stop");
             inProximity = true;
         }
-
-        return this;
-    }
-
-    public void drawRoute(DirectionsRoute currentRoute, String color) {
-
-        List<Waypoint> waypoints = currentRoute.getGeometry().getWaypoints();
-
-        LatLng[] point = new LatLng[waypoints.size()];
-        for (int i = 0; i < waypoints.size(); i++) {
-            point[i] = new LatLng(waypoints.get(i).getLatitude(),
-                                  waypoints.get(i).getLongitude());
-        }
-
-
-        polylineToNextStop = new PolylineOptions().add(point)
-                .color(Color.parseColor(color))
-                .width(5);
-
-        mapView.addPolyline(polylineToNextStop);
-
-    }
-
-    public boolean removePolyline(PolylineOptions polylineOptions){
-        boolean removed = false;
-        if (polylineOptions != null){
-            mapView.removeAnnotation(polylineOptions.getPolyline());
-            removed = true;
-            return removed;
-        }
-        return removed;
-    }
-
-    public PolylineOptions getPolylineToNextStop() {
-        return polylineToNextStop;
-    }
-
-    public Router getRoute() {
-
-        List<Waypoint> waypoints = (directionsRoute != null)
-                ? checkWaypointProximity().updateWaypointsRemaining().getCurrentRoute()
-                : updateWaypointsRemaining().getCurrentRoute();
-
-        MapboxDirections md = new MapboxDirections.Builder()
-                .setAccessToken(MAPBOX_ACCESS_TOKEN)
-                .setWaypoints(waypoints)
-                .setProfile(DirectionsCriteria.PROFILE_DRIVING)
-                .build();
-
-        md.enqueue(new Callback<DirectionsResponse>() {
-
-            @Override
-            public void onResponse(Response<DirectionsResponse> response, Retrofit retrofit) {
-                CallCounter.count();
-
-                printResponseMessage(response);
-
-                directionsRoute = response.body().getRoutes().get(0);
-                checkWaypointProximity();
-
-                drawRoute(directionsRoute, "#ff0000");
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-                Log.e(LOG_TAG, "getRoute-Error: " + t.getMessage());
-            }
-        });
 
         return this;
     }
@@ -159,12 +110,148 @@ public final class Router {
         }
     }
 
-    public MapView getMapView() {
-        return mapView;
+    private void showMessage(String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void checkOffRoute(Waypoint target) {
+        if (fullRoute.isOffRoute(target)) {
+            showMessage("You are off-route.");
+        } else {
+            showMessage("You are not off-route.");
+        }
+    }
+
+    public void drawRoute(DirectionsRoute directionsRoute, String color) {
+
+        List<Waypoint> waypoints = directionsRoute.getGeometry().getWaypoints();
+
+        LatLng[] point = new LatLng[waypoints.size()];
+        for (int i = 0; i < waypoints.size(); i++) {
+            point[i] = new LatLng(waypoints.get(i).getLatitude(),
+                    waypoints.get(i).getLongitude());
+        }
+
+        polylineToNextStop = new PolylineOptions()
+                .add(point)
+                .color(Color.parseColor(color))
+                .width(5);
+
+        mapView.addPolyline(polylineToNextStop);
+
+    }
+
+    public boolean removePolyline(PolylineOptions polylineOptions) {
+
+        boolean removed = false;
+
+        if (polylineOptions != null){
+            mapView.removeAnnotation(polylineOptions.getPolyline());
+            removed = true;
+
+            return removed;
+        }
+
+        return removed;
+    }
+
+    public List<Waypoint> getCurrentRoute() {
+
+        List<Waypoint> positionAndNextWaypoint = new ArrayList<>();
+
+        positionAndNextWaypoint.add(new Waypoint(currentLocation.getLongitude(), currentLocation.getLatitude()));
+        positionAndNextWaypoint.add(waypointsRemaining.get(0));
+
+        return positionAndNextWaypoint;
+    }
+
+    public Router loadFullRoute() {
+
+        MapboxDirections md = new MapboxDirections.Builder()
+                .setAccessToken(MAPBOX_ACCESS_TOKEN)
+                .setWaypoints(fewerWaypoints())
+                .setProfile(DirectionsCriteria.PROFILE_DRIVING)
+                .build();
+
+        md.enqueue(new Callback<DirectionsResponse>() {
+
+            @Override
+            public void onResponse(Response<DirectionsResponse> response, Retrofit retrofit) {
+                CallCounter.count();
+
+                printResponseMessage(response);
+
+                fullRoute = response.body().getRoutes().get(0);
+
+                showMessage(String.format("Route is %d meters long.", fullRoute.getDistance()));
+
+                drawRoute(fullRoute, "#3887be");
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e(LOG_TAG, "loadFullRoute-Error: " + t.getMessage());
+                showMessage("loadFullRoute-Error: " + t.getMessage());
+            }
+        });
+
+        return this;
+    }
+
+    public Router loadRoute() {
+
+        List<Waypoint> waypoints = (currentRoute != null)
+                ? checkWaypointProximity().updateWaypointsRemaining().getCurrentRoute()
+                : updateWaypointsRemaining().getCurrentRoute();
+
+        MapboxDirections md = new MapboxDirections.Builder()
+                .setAccessToken(MAPBOX_ACCESS_TOKEN)
+                .setWaypoints(waypoints)
+                .setProfile(DirectionsCriteria.PROFILE_DRIVING)
+                .build();
+
+        md.enqueue(new Callback<DirectionsResponse>() {
+
+            @Override
+            public void onResponse(Response<DirectionsResponse> response, Retrofit retrofit) {
+                CallCounter.count();
+
+                printResponseMessage(response);
+
+                currentRoute = response.body().getRoutes().get(0);
+
+                checkWaypointProximity();
+
+                drawRoute(currentRoute, "#ff0000");
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e(LOG_TAG, "loadRoute-Error: " + t.getMessage());
+                showMessage("loadRoute-Error: " + t.getMessage());
+            }
+        });
+
+        return this;
+    }
+
+    public List<Waypoint> getWaypoints() {
+        return waypoints;
+    }
+
+    public PolylineOptions getPolylineToNextStop() {
+        return polylineToNextStop;
+    }
+
+    public Router setWaypointsRemaining(List<Waypoint> waypoints) {
+        waypointsRemaining = waypoints;
+
+        return this;
     }
 
     public Router setCurrentLocation(Location currentLocation) {
         this.currentLocation = currentLocation;
+
         return this;
     }
 }
